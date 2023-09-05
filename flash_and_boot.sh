5cd93b25bbbf27d13b2e88d3ba4ee4aeaf146690
Gabriel Marcano <gabemarcano@yahoo.com>
2023-09-05 11:57:25 -0700

    flash_and_boot.sh: remove hardcoded filename

--- flash_and_boot.sh
+++ flash_and_boot.sh
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Gabriel Marcano, 2023
 
-fastboot flash boot hack_bootimg.bin
-fastboot boot hack_bootimg.bin
+fastboot flash boot $@
+fastboot boot $@
